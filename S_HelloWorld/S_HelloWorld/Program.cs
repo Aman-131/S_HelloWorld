78584d2e4593b5d77d9ec7b0f97c763309274cbf
Aman-131 <aman.ssingh@outlook.com>
2020-01-22 00:17:57 -0500

    The first Commit

--- S_HelloWorld/S_HelloWorld/Program.cs
+++ S_HelloWorld/S_HelloWorld/Program.cs
@@ -7,6 +7,7 @@ namespace S_HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Updated Code");
         }
     }
 }
